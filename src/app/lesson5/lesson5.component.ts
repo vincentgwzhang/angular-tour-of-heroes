35e25a43ae6ce9449017b182df2159bac3ebfb04
Vincent Zhang <v.zhang@aspanetconomy.net>
2021-01-22 12:32:08 +0100

    Add the select option

--- src/app/lesson5/lesson5.component.ts
+++ src/app/lesson5/lesson5.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-lesson5',
   templateUrl: './lesson5.component.html',
@@ -9,9 +10,20 @@ export class Lesson5Component implements OnInit {
 
   color = 'green';
 
-  constructor() { }
+  heroes: Hero[];
+
+  hero: Hero;
+
+  constructor() {
+    this.heroes = HEROES;
+  }
 
   ngOnInit(): void {
+  }
+
+  onChangeSelect() : void{
+    // Every time select, the hero will change
+    console.log(this.hero.id);
   }
 
 }
